38ff133d5b684e40533b4e117969f6890a2f5a15
altercode <p.wajcowicz@gmail.com>
2024-08-26 12:00:19 +0200

    Wykonanie zadania 5.

--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,24 @@
 console.log('DOM');
 
 const curr = document.querySelector('.js-curr');
+
+const deleteButton = document.createElement('button')
+deleteButton.textContent = 'usuń z koszyka'
+curr.insertAdjacentElement('afterend', deleteButton)
+
+curr.parentNode.childNodes.forEach(element => {
+	element.classList && ![...element.classList].includes('js-curr') && element.classList.add("siblings");
+});
+
+curr.parentElement.nextElementSibling.setAttribute('title', 'nextElementSibling');
+
+const newPElement = document.createElement('p')
+newPElement.textContent = 'To jest nowy paragraf.'
+
+curr.parentElement.parentElement.lastElementChild.querySelector('.article__btn').insertAdjacentElement('beforebegin', newPElement);
+
+const newArticleElement = curr.parentElement.nextElementSibling.cloneNode(true);
+newArticleElement.querySelector('.article__title').textContent = 'To jest nowy artykuł!'
+newArticleElement.querySelector('.article__description').textContent = 'To jest treść nowego artykułu!'
+
+curr.parentElement.parentElement.prepend(newArticleElement);
