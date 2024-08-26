5fd0f3cedd5b4a98bf2215b5fc69697d353246b6
altercode <p.wajcowicz@gmail.com>
2024-08-26 11:08:48 +0200

    Wykonanie zadania 3.

--- 03/app.js
+++ 03/app.js
@@ -12,3 +12,16 @@ const buttonSettings = {
     },
     text: 'Click me!',
 }
+
+const parentForButtonElement = document.querySelector('.parent-for-button')
+const buttonElement = document.createElement('button', buttonSettings)
+
+for (attr in buttonSettings.attr) {
+    buttonElement.setAttribute(attr, buttonSettings.attr[attr])
+}
+for (style in buttonSettings.css) {
+    buttonElement.style[style] = buttonSettings.css[style];
+}
+buttonElement.textContent = buttonSettings.text
+
+parentForButtonElement.appendChild(buttonElement);
